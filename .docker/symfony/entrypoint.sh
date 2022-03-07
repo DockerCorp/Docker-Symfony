b9fd52cc6db6b54dff57c9c6834fb97a5d24b871
Julien Bourgeois <92128822+JulienBrgs@users.noreply.github.com>
2022-03-07 06:18:35 +0100

    Update entrypoint to handle empty app directory

--- .docker/symfony/entrypoint.sh
+++ .docker/symfony/entrypoint.sh
@@ -3,19 +3,38 @@
 echo "######################"
 echo "## Package versions ##"
 echo "######################"
+echo ""
 php -v | grep PHP | head -1
 composer -V
 symfony -V
 echo -e "\n"
 
-echo "###########################"
-echo "## Preparing Symfony ... ##"
-echo "###########################"
-composer install
-echo -e "\n"
+if [ -z "$(ls -A /var/www/app)" ]; then
+  # Empty
+  echo "############################################"
+  echo "## Unable to find the Symfony project ... ##"
+  echo "############################################"
+  echo ""
+  echo "1. You need to SSH into the container"
+  echo "2. You need to create a new Symfony project"
+  echo "3. You need to restart the container"
+  echo ""
+  echo "############################################"
+  echo -e "\n"
+  tail -f /dev/null
+else
+  # Not Empty
+  echo "###########################"
+  echo "## Preparing Symfony ... ##"
+  echo "###########################"
+  echo ""
+  composer install
+  echo -e "\n"
 
-echo "##########################"
-echo "## Starting Symfony ... ##"
-echo "##########################"
-symfony serve --no-tls
-echo -e "\n"
+  echo "##########################"
+  echo "## Starting Symfony ... ##"
+  echo "##########################"
+  echo ""
+  symfony serve --no-tls
+  echo -e "\n"
+fi
